17eaf77f5328842880c17568b656f3af3848dbb6
Ryan <ryanpantk@gmail.com>
2023-11-21 14:23:33 +0800

    feat: added mqtt function

--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -13,24 +13,101 @@ import json
 from config import *
 from pubsub import *
 
-# Callback when the subscribed topic receives a message
-def on_message_received(topic, payload, dup, qos, retain, **kwargs):
-    print("Received message from topic '{}': {}".format(topic, payload))
-
-def main():
+"""
+RPI send device ID and passcode to AWS IOT Core for Authentication (Sean)
+"""
+def validate(deviceId, passcode, imageURL=None):
     # Create a MQTT connection 
     mqtt_connection = connectToIOTCore()
-
-    TOPIC = ESP32_TAKE_PHOTO_TOPIC
-    PAYLOAD = {"deviceId" : "Test", "passCode": "123456"}
-
-    # Subscribe to the topic
-    subscribeToIOTCore(mqtt_connection, ESP32_TAKE_PHOTO_TOPIC, on_message_received)
-
+    TOPIC = AWS_VALIDATE_TOPIC
+    PAYLOAD = {"deviceId" : deviceId, "passcode": passcode}
     # Publish to the topic
-    publishToIOTCore(mqtt_connection, ESP32_TAKE_PHOTO_TOPIC, PAYLOAD)
-
+    publishToIOTCore(mqtt_connection, TOPIC, PAYLOAD)
     # Disconnect
     disconnectFromIOTCore(mqtt_connection)
 
-main()
+"""
+RPI send device ID and passcode to ESP32 (Sean)
+"""
+def takePhoto(deviceId, passcode):
+    # Create a MQTT connection 
+    mqtt_connection = connectToIOTCore()
+    TOPIC = ESP32_TAKE_PHOTO_TOPIC
+    PAYLOAD = {"deviceId" : deviceId, "passcode": passcode}
+    # Publish to the topic
+    publishToIOTCore(mqtt_connection, TOPIC, PAYLOAD)
+    # Disconnect
+    disconnectFromIOTCore(mqtt_connection)
+
+"""
+ESP32 send device ID, passcode and imageURL to AWS IOT Core to edit order in dynamoDB (Ryan)
+"""
+def uploadPhoto(deviceId, passcode, imageURL):
+    # Create a MQTT connection 
+    mqtt_connection = connectToIOTCore()
+    TOPIC = ESP32_PUBLISH_PHOTO_TOPIC
+    PAYLOAD = {"deviceId" : deviceId, "passcode": passcode, "imageURL": imageURL}
+    # Publish to the topic
+    publishToIOTCore(mqtt_connection, TOPIC, PAYLOAD)
+    # Disconnect
+    disconnectFromIOTCore(mqtt_connection)
+    
+"""
+AWS inform RPI on the status of the validation (Sean)
+"""
+def subscribeToValidatedTopic():
+    # Callback when the subscribed topic receives a message
+    def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+        print("Received message from topic '{}': {}".format(topic, payload))
+
+    # Create a MQTT connection 
+    mqtt_connection = connectToIOTCore()
+    TOPIC = AWS_VALIDATED_TOPIC
+    # Subscribe to the topic
+    subscribeToIOTCore(mqtt_connection, TOPIC, on_message_received)
+    # Disconnect
+    disconnectFromIOTCore(mqtt_connection)
+
+"""
+AWS inform ESP32 that the photo has been uploaded (Ryan)
+"""
+def subscribeToPhotoPublishedTopic():
+    # Callback when the subscribed topic receives a message
+    def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+        print("Received message from topic '{}': {}".format(topic, payload))
+
+    # Create a MQTT connection 
+    mqtt_connection = connectToIOTCore()
+    TOPIC = ESP32_PHOTO_PUBLISHED_TOPIC
+    # Subscribe to the topic
+    subscribeToIOTCore(mqtt_connection, TOPIC, on_message_received)
+    # Disconnect
+    disconnectFromIOTCore(mqtt_connection)
+
+
+"""
+ESP32 inform RPI that the photo has been uploaded (Sean)
+"""
+def subscribeToPhotoUploadedTopic():
+    # Callback when the subscribed topic receives a message
+    def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+        print("Received message from topic '{}': {}".format(topic, payload))
+
+    # Create a MQTT connection 
+    mqtt_connection = connectToIOTCore()
+    TOPIC = ESP32_PHOTO_UPLOADED_TOPIC
+    # Subscribe to the topic
+    subscribeToIOTCore(mqtt_connection, TOPIC, on_message_received)
+    # Disconnect
+    disconnectFromIOTCore(mqtt_connection)
+
+deviceID = "Raspberry-Pi"
+passcode = "123456"
+imageURL = "test"
+
+# validate(deviceID, passcode)
+takePhoto(deviceID, passcode)
+# uploadPhoto(deviceID, passcode, imageURL)
+# subscribeToValidatedTopic()
+# subscribeToPhotoUploadedTopic()
+# subscribeToPhotoPublishedTopic()
