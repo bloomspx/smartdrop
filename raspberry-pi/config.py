53573ec7a64129cb36ba0cd5995615ed203a0028
Ryan <ryanpantk@gmail.com>
2023-11-21 13:19:14 +0800

    feat: update mqtt topics

--- raspberry-pi/config.py
+++ raspberry-pi/config.py
@@ -6,7 +6,9 @@ PATH_TO_PRIVATE_KEY = "certificates/f602ee93434ed4a581339e1cee4ae277c0834ef4e4dd
 PATH_TO_AMAZON_ROOT_CA_1 = "certificates/AmazonRootCA1.pem"
 
 # AWS MQTT Topics
-AWS_VALIDATE_TOPIC = "cciot/validate"
-AWS_VALIDATED_TOPIC = "cciot/validated"
-ESP32_TAKE_PHOTO_TOPIC = "cciot/take-photo"
-ESP32_PHOTO_UPLOADED_TOPIC = "cciot/photo-uploaded"
+AWS_VALIDATE_TOPIC = "cciot/validate" #rpi pub, cloud iot core sub  - deviceID, passcode
+AWS_VALIDATED_TOPIC = "cciot/validated" #cloud iot core pub, rpi sub
+ESP32_TAKE_PHOTO_TOPIC = "cciot/take-photo" #rpi pub, esp32 sub - deviceID, passcode
+ESP32_PHOTO_UPLOADED_TOPIC = "cciot/photo-uploaded" #esp32 pub, rpi sub
+ESP32_PUBLISH_PHOTO_TOPIC = "cciot/publish-photo" #esp32 pub, cloud iot core sub - imageURL, deviceID, passcode
+ESP32_PHOTO_PUBLISHED_TOPIC = "cciot/photo-published" #cloud iot core pub, esp32 sub
