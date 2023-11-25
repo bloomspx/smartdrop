ec37e4500dd4a8811ed70e9bfefcc91c7188bb67
ssjh23 <64569228+ssjh23@users.noreply.github.com>
2023-11-25 22:08:54 +0800

    fix: Updated state diagram for simulation

--- hardware/cciot_rpi/cciot_rpi_programs/state_machine.py
+++ hardware/cciot_rpi/cciot_rpi_programs/state_machine.py
@@ -6,6 +6,7 @@ class LockState(Enum):
     UNLOCKED = 2
 
 class ProcessState(Enum):
+    START_DELIVERY_SEQUENCE = 0,
     WAITINGTOUNLOCKBOX = 1,
     TAKINGORDERPICTURE = 2,
     KEYINGINORDERS = 3,
@@ -18,8 +19,8 @@ class LimitSwitchState(Enum):
     CLOSED = "closed"
 
 lock_state = LockState.LOCKED
-process_state = ProcessState.WAITINGTOUNLOCKBOX
-passcode = "4789"
+process_state = ProcessState.START_DELIVERY_SEQUENCE
+passcode = ["4789", "1234", "2345"]
 limit_switch_state = LimitSwitchState.CLOSED
 
 def unlock():
@@ -33,10 +34,16 @@ def lock():
 def state_machine():
     global lock_state
     global process_state
-    if process_state == ProcessState.WAITINGTOUNLOCKBOX and lock_state == LockState.LOCKED:
+    if process_state == ProcessState.START_DELIVERY_SEQUENCE:
+        user_input = input("Press # to start delivery sequence: ")
+        if user_input == "#":
+            process_state = ProcessState.WAITINGTOUNLOCKBOX
+        else:
+            user_input = ""
+    elif process_state == ProcessState.WAITINGTOUNLOCKBOX:
         user_input = input("Enter order passcode: ")
         # Insert PUB passcode and receive confirmation from MQTT here
-        if user_input == passcode:
+        if user_input in passcode:
             print("Correct passcode")
             unlock()
             process_state = ProcessState.TAKINGORDERPICTURE
@@ -44,52 +51,53 @@ def state_machine():
             lock()
             print("Incorrect passcode, please key in again")
     elif process_state == ProcessState.TAKINGORDERPICTURE:
-        print("Hold up your order to the camera")
+        print("Place the item in the box")
         user_input = input("Press # to take picture")
         if user_input == "#":
             # Wait for S3 response that picture has been uploaded
             print("Picture taken")
-            process_state = ProcessState.KEYINGINORDERS
         else:
+            user_input = ""
             print("Please press # to take picture")
         process_state = ProcessState.CONFIRMINGMOREORDERS
     elif process_state == ProcessState.CONFIRMINGMOREORDERS:
-        user_input = input("Do you have more orders? (y/n) ")
-        if user_input == "y":
+        user_input = input("Do you have more orders? Press # to key in more orders, * to lock box ")
+        if user_input == "#":
             process_state = ProcessState.KEYINGINORDERS
-        else:
+        elif user_input == "*":
             print("Thank you, please close the door")
-            process_state = ProcessState.WAITINGTOLOCKBOX
+            process_state = ProcessState.CONFIRMLOCKSEQUENCE
+        else: 
+            # Always clear the user input if the user presses any other button
+            user_input = ""
     elif process_state == ProcessState.KEYINGINORDERS:
+        print("Key in additional order passcodes. To lock the box, empty the input and press *")
         user_input = input("Enter order passcode: ")
-        if user_input == passcode:
+        if user_input in passcode:
             print("Correct passcode")
             unlock()
             process_state = ProcessState.TAKINGORDERPICTURE
-        else:
-            # User may want to break out of keying in sequence and just lock the door. This is the difference between the initial WAITINGTOUNLOCKBOX State
-            print("Incorrect passcode, please key in again or press # to begin locking sequence")
-            if user_input == "#":
-                process_state = ProcessState.CONFIRMLOCKSEQUENCE
-            else:
-                process_state = ProcessState.KEYINGINORDERS
+        elif user_input == "*":
+            process_state = ProcessState.CONFIRMLOCKSEQUENCE
     elif process_state == ProcessState.CONFIRMLOCKSEQUENCE:
-        user_input = input("Press # to confirm locking sequence, * to return to keying in orders")
-        if user_input == "#":
+        user_input = input("Press * to confirm locking the box, # to return to keying in orders")
+        if user_input == "*":
             lock()
             process_state = ProcessState.WAITINGTOLOCKBOX
-        else:
+        elif user_input == "#":
             process_state = ProcessState.KEYINGINORDERS
+        else: 
+            # Always clear the user input if the user presses any other button
+            user_input = ""
     elif process_state == ProcessState.WAITINGTOLOCKBOX:
         # Simulate the limit switch being in contact with the door 
         limit_switch_state = input("Is the door closed? (closed/open)")
         if limit_switch_state == LimitSwitchState.CLOSED.value:
-            user_input = input("Press # to lock")
-            if user_input == "#":
-                lock()
-                process_state = ProcessState.WAITINGTOUNLOCKBOX
+            lock()
+            process_state = ProcessState.START_DELIVERY_SEQUENCE
         else:
             print("Please close the door properly")
+
 
 while True:
     state_machine()
