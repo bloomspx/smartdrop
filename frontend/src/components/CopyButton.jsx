f2bade05f02ab0337d2d1361c52560434cc35d17
renhangx <liurenhang0@gmail.com>
2023-11-28 18:08:36 +0800

    adjust dashboard alignment

--- frontend/src/components/CopyButton.jsx
+++ frontend/src/components/CopyButton.jsx
@@ -29,13 +29,13 @@ const CopyButton = ({ textToCopy }) => {
   };
 
   return (
-    <div className="w-full max-w-xs mx-auto">
-      <div className="text-purple-700 p-3 rounded-t-lg border border-purple-500">
+    <div className="w-full max-w-xs mx-auto h-auto">
+      <div className="text-purple-700 p-2 rounded-t-lg border border-purple-500">
         Copy delivery instruction template with passcode
       </div>
       <button 
         onClick={handleCopy} 
-        className="bg-purple-500 text-white p-3 rounded-b-lg w-full border border-purple-500 hover:bg-purple-600 focus:outline-none focus:bg-purple-600 transition-colors"
+        className="bg-purple-500 text-white p-2 rounded-b-lg w-full border border-purple-500 hover:bg-purple-600 focus:outline-none focus:bg-purple-600 transition-colors"
       >
         Copy
       </button>
--- frontend/src/components/ParcelItem.jsx
+++ frontend/src/components/ParcelItem.jsx
@@ -9,11 +9,11 @@ const ParcelItem = ({ item }) => {
   return (
     <div className={`flex justify-between items-center mb-4 p-4 border-l-4 ${borderColorClass} bg-white rounded-lg shadow`}>
       {/* Left column: Item name, date, number of days, status */}
-      <div className="flex flex-col justify-between flex-grow">
-        <h3 className="font-bold truncate">{item.productName}</h3>
-        <p className="text-sm">{item.isDelivered ? `Date Delivered: ${item.dateDelivered}` : `Date Ordered: ${item.dateOrdered}`}</p>
-        <p className="text-sm">Number of Days: {item.days}</p>
-        <div className="flex items-center">
+      <div className="relative flex flex-col justify-between flex-grow">
+        <h3 className="font-bold top left truncate">{item.productName}</h3>
+        <p className="text-sm py-1">{item.isDelivered ? `Date Delivered: ${item.dateDelivered}` : `Date Ordered: ${item.dateOrdered}`}</p>
+        <p className="text-sm py-1">Number of Days: {item.days}</p>
+        <div className="flex items-center pt-3">
           <span className={`text-sm font-bold ${item.isDelivered ? 'text-green-500' : 'text-yellow-500'}`}>
             {statusIcon}
           </span>
@@ -23,16 +23,16 @@ const ParcelItem = ({ item }) => {
 
       {/* Middle column: Time delivered, ordered from */}
       <div className="flex flex-col justify-between flex-grow">
-        {item.isDelivered && <p className="text-sm">Time Delivered: {item.timeDelivered}</p>}
-        {!item.isDelivered && <p className='text-sm'>Passcode: {item.passcode}</p>}
-        <p className="text-sm">Ordered From: {item.orderedFrom}</p>
+        {item.isDelivered && <p className="text-sm py-1">Time Delivered: {item.timeDelivered}</p>}
+        {!item.isDelivered && <p className='text-sm py-1'>Passcode: {item.passcode}</p>}
+        <p className="text-sm py-1">Ordered From: {item.orderedFrom}</p>
       </div>
 
       {/* Right column: Image or Copy button */}
       {/* <div className="flex items-stretch justify-end flex-grow"> */}
       <div className="flex flex-col justify-between flex-grow">
         {item.isDelivered ? (
-          <img src={item.imageSrc} alt={`Delivery ${item.productName}`} className="w-28 h-28 object-cover rounded" />
+          <img src={item.imageSrc} alt={`Delivery ${item.productName}`} className="w-2 h-28 object-cover rounded" />
         ) : (
         <CopyButton textToCopy={item.passcode}/>
         )}
