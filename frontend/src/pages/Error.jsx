9fc9f9741c2fc1d6dcb873adffdc866ceae93c73
bloomspx <bloomspx@gmail.com>
2023-11-29 12:44:53 +0800

    fix: update error page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom"
+
 const Error = () => {
     
     return (
-        <div className="error-container">
-            <h2 className="wordstyle">404 Not Found</h2>
+        <div className="flex flex-col justify-center items-center h-screen w-screen bg-[#EBFEFA]">
+            <h1 className="font-bold text-4xl text-center">404 Not Found</h1>
+            <p className="mt-4 text-center text-black">
+                Navigate to SmartDrop homepage&nbsp;
+                <Link to="/" className="text-purple-600 hover:text-purple-700 font-semibold underline">here</Link>
+            </p>
         </div>
     )
 }
