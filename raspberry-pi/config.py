0cb8e3df413a15a05271343e83bb7ed51b8a9919
Ryan <ryanpantk@gmail.com>
2023-11-30 16:11:00 +0800

    add absolute path

--- raspberry-pi/config.py
+++ raspberry-pi/config.py
@@ -1,9 +1,9 @@
 # AWS IOT Core Configurations
 ENDPOINT = "a3f2k66wa7dxq0-ats.iot.ap-southeast-1.amazonaws.com"
 CLIENT_ID = "Raspberry-Pi"
-PATH_TO_CERTIFICATE = "/certificates/f602ee93434ed4a581339e1cee4ae277c0834ef4e4dd25a03a2c3069d71675b6-certificate.pem.crt"
-PATH_TO_PRIVATE_KEY = "/certificates/f602ee93434ed4a581339e1cee4ae277c0834ef4e4dd25a03a2c3069d71675b6-private.pem.key"
-PATH_TO_AMAZON_ROOT_CA_1 = "/certificates/AmazonRootCA1.pem"
+PATH_TO_CERTIFICATE = "/home/pgd/iot-smart-delivery-box/raspberry-pi/certificates/f602ee93434ed4a581339e1cee4ae277c0834ef4e4dd25a03a2c3069d71675b6-certificate.pem.crt"
+PATH_TO_PRIVATE_KEY = "/home/pgd/iot-smart-delivery-box/raspberry-pi/certificates/f602ee93434ed4a581339e1cee4ae277c0834ef4e4dd25a03a2c3069d71675b6-private.pem.key"
+PATH_TO_AMAZON_ROOT_CA_1 = "/home/pgd/iot-smart-delivery-box/raspberry-pi/certificates/AmazonRootCA1.pem"
 
 # AWS MQTT Topics
 AWS_VALIDATE_TOPIC = "cciot/validate" #rpi pub, cloud iot core sub  - deviceID, passcode
